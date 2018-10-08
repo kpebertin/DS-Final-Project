e5f7880b333ddadfee36b216de3033d76384a35e
Kyle Ebertin <kebertin@indiana.edu>
2018-10-07 21:15:27 -0400

    corrected one typo in common.php file

--- app/common.php
+++ app/common.php
@@ -2,7 +2,7 @@
 
 chdir(__DIR__);
 set_include_path (__DIR__);
-if ($SERVER['REQUEST_METHOD'] == 'POST' && stripos($_SERVER['CONTENT_TYPE'], 'application/json') !== false) {
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && stripos($_SERVER['CONTENT_TYPE'], 'application/json') !== false) {
     $_POST = json_decode(file_get_contents('php://input'), true);
 }
 require 'environment.php';
