341b769a10b68d602aa4109d8cd6cbf61bd03724
Kyle Ebertin <kebertin@indiana.edu>
2018-10-11 15:32:11 -0400

    fied typo

--- public/api/sensorDeployedData.php
+++ public/api/sensorDeployedData.php
@@ -6,7 +6,7 @@ $aTurbineDeployedID = $_GET['turbineDeployedID'] ?? "";
 
 #echo $aClientID;
 
-$sensorDeployedArray = SensorDeployedClass::getTurbineDeployedData($aTurbineDeployedID);
+$sensorDeployedArray = SensorDeployedClass::getSensorDeployedData($aTurbineDeployedID);
 $json = json_encode($sensorDeployedArray);
 header('Content-Type: application-json');
 echo $json;
