773c1b3c101e85113c4c7bb6b87d8b0db334d0fc
Kyle Ebertin <kebertin@indiana.edu>
2018-10-11 15:37:27 -0400

    fied typo

--- public/api/timeSeriesData.php
+++ public/api/timeSeriesData.php
@@ -5,6 +5,6 @@ require '../../app/common.php';
 $aDeployedSensorID = $_GET['sensorDeployedID'] ?? "";
 
 $timeSeriesDataArray = TimeSeriesDataClass::getTimeSeriesData($aDeployedSensorID);
-$json = json_encode($timeSeriesDataArrayArray);
+$json = json_encode($timeSeriesDataArray);
 header('Content-Type: application-json');
 echo $json;
