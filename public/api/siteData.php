6435b40908403e3ee23291e72b089e97401d6153
Kyle Ebertin <kebertin@indiana.edu>
2018-10-11 01:36:42 -0400

    adding sites

--- public/api/siteData.php
+++ public/api/siteData.php
@@ -4,6 +4,8 @@ require '../../app/common.php';
 
 $aClientID = $_GET['clientID'] ?? "";
 
+echo $aClientID;
+
 $siteArray = SiteClass::getSiteData($aClientID);
 $json = json_encode($siteArray);
 header('Content-Type: application-json');
