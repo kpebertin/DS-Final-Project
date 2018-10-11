9b7b9bdf52d10742ad12aba6efc45ad799ad86a6
ecowger1229 <42587866+ecowger1229@users.noreply.github.com>
2018-10-11 16:24:55 -0400

    add if statement

--- public/api/siteData.php
+++ public/api/siteData.php
@@ -2,6 +2,11 @@
 
 require '../../app/common.php';
 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    require 'postSiteData.php';
+    die;
+}
+
 $aSiteID = $_GET['siteID'] ?? "";
 
 #echo $aClientID;
