86ab0223502be6d21b4f4de0089eac94eda689f2
ecowger1229 <42587866+ecowger1229@users.noreply.github.com>
2018-10-11 14:43:55 -0400

    update turbines deployed class

---
+++ app/models/TurbineDeployedClass.php
@@ -0,0 +1,38 @@
+<?php
+class TurbineDeployedClass {
+    public $turbineDeployedID;
+    public $turbineID;
+    public $siteID;
+    public $serialNumber;
+    public $deployedDate;
+    public $totalFiredHours;
+    public $totalStarts;
+    public $lastPlannedOutageDate;
+    public $lastUnplannedOutageDate;
+
+    public function __construct($row) {
+        $this->turbineDeployedID = isset($row['turbineDeployedID']) ? $row['turbineDeployedID'] : null;
+        $this->turbineID = isset($row['turbineID']) ? $row['turbineID'] : null;
+        $this->siteID = isset($row['siteID']) ? $row['siteID'] : null;
+        $this->serialNumber = isset($row['serialNumber']) ? $row['serialNumber'] : null;
+        $this->deployedDate = isset($row['deployedDate']) ? $row['deployedDate'] : null;
+        $this->totalFiredHours = isset($row['totalFiredHours']) ? $row['totalFiredHours'] : null;
+        $this->totalStarts = isset($row['totalStarts']) ? $row['totalStarts'] : null;
+        $this->lastPlannedOutageDate = isset($row['lastPlannedOutageDate']) ? $row['lastPlannedOutageDate'] : null;
+        $this->lastUnplannedOutageDate = isset($row['lastUnplannedOutageDate']) ? $row['lastUnplannedOutageDate'] : null;
+
+    }
+
+    public static function getTurbineDeployedData() {
+        $db = new PDO(DB_SERVER, DB_USER, DB_PW);
+        $sql = 'SELECT * FROM TurbineDeployed;';
+        $pdoStatement = $db->prepare($sql);
+        $connection = $pdoStatement->execute([]);
+        $arrayOfSensors = [];
+        while ($row = $pdoStatement->fetch(PDO::FETCH_ASSOC)) {
+            $aClient = new TurbineDeployedClass($row);
+            array_push($arrayOfTurbinesDeployed, $aTurbine);
+        }
+        return $arrayOfTurbinesDeployed;
+    }
+}
