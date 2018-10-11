679c7113fbf1e838d7dca0756077edc79d40c83f
Kyle Ebertin <kebertin@indiana.edu>
2018-10-11 15:43:39 -0400

    fied typo

--- public/api/turbineDeployedData.php
+++ public/api/turbineDeployedData.php
@@ -6,7 +6,7 @@ $aSiteID = $_GET['siteID'] ?? "";
 
 #echo $aClientID;
 
-$siteArray = TurbinesDeployedClass::getTurbineDeployedData($aSiteID);
+$siteArray = TurbineDeployedClass::getTurbineDeployedData($aSiteID);
 $json = json_encode($siteArray);
 header('Content-Type: application-json');
 echo $json;
