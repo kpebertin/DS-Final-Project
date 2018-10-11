959236d53f7f7fe788b846b9b9248c21e89ab6c9
Kyle Ebertin <kebertin@indiana.edu>
2018-10-11 02:22:21 -0400

    fixing bug

--- public/js/dashboardApp.js
+++ public/js/dashboardApp.js
@@ -49,7 +49,7 @@ var dashboardApp = new Vue ({
         //console.log(this.clients);
         console.log(JSON.stringify(this.clients));
         console.log(JSON.stringify(this.clients[0]));
-        this.setActiveClient(this.clients[0]);
+        this.setActiveClient(this.clients["0"]);
         //console.log(this.activeClient);
         //this.fetchSites(this.activeClient.clientID);
         // Currently use the below line instead of the above since client 1 has no sites which should be changed.
