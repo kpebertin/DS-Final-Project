257fb283d39cd4aa362208e748e8648d7d84e1f3
shrbhatt <shrbhatt@indiana.edu>
2018-10-23 23:22:32 -0400

    Fixing typo

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -11,7 +11,7 @@ function openTurbineTab(evt, turbineID) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
 
-    document.getElementById(siteID).style.display = "block";
+    document.getElementById(turbineID).style.display = "block";
     evt.currentTarget.className += " active";
 }
 
