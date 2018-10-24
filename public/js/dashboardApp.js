95d8434dd684756c6910507bc18c45ef519f303d
shrbhatt <shrbhatt@indiana.edu>
2018-10-24 00:24:32 -0400

    fixing logic

--- public/js/dashboardApp.js
+++ public/js/dashboardApp.js
@@ -43,7 +43,7 @@ var dashboardApp = new Vue ({
             })
         )},
         newActiveClient: function(c) {
-            dashboardApp.activeClient = dashboardApp.clients[Number(c) - 1];
+            dashboardApp.activeClient = dashboardApp.clients[Number(c)];
             dashboardApp.fetchSites(dashboardApp.activeClient['clientID']);
         }
     },
