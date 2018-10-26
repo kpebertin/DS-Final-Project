045b7c61b3b583ff0628fb9da0a478ba727a8a13
Kyle Ebertin <kyle78@ebertinhome.net>
2018-10-25 22:55:49 -0400

    changed html

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,38 +1,20 @@
-var openTurbineTab = function(evt, turbineID) {
-    var i, tabcontentTurbine, tablinksTurbine;
+var openTab = function(evt, turbineID) {
+    var i, tabcontent, tablinks;
     
-    tabcontentTurbine = document.getElementsByClassName("tabcontentTurbine");
-    for (i = 0; i < tabcontentTurbine.length; i++) {
-        tabcontentTurbine[i].style.display = "none";
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
     }
 
-    tablinksTurbine = document.getElementsByClassName("tabLinkTurbine");
-    for (i = 0; i < tablinksTurbine.length; i++) {
-        tablinksTurbine[i].className = tablinksTurbine[i].className.replace(" active", "");
+    tablinks = document.getElementsByClassName("tabLink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
 
     document.getElementById(turbineID).style.display = "block";
     evt.currentTarget.className += " active";
 }
 
-var openSiteTab = function(evt, siteID) {
-    var i, tabcontentSite, tablinksSite;
-
-    tabcontentSite = document.getElementsByClassName("tabcontentSite");
-    for (i = 0; i < tabcontentSite.length; i++) {
-        tabcontentSite[i].style.display = "none";
-    }
-
-    tablinksSite = document.getElementsByClassName("tabLinkSite");
-    for (i = 0; i < tablinksSite.length; i++) {
-        tablinksSite[i].className = tablinksSite[i].className.replace(" active", "");
-    }
-
-    document.getElementById(siteID).style.display = "block";
-    evt.currentTarget.className += " active";
-}
-
 function onloadDefault() {
-    document.getElementById("defaultOpenSite").click();
-    document.getElementById("defaultOpenTurbine").click();
+    document.getElementById("defaultOpen").click();
 }
