d67cfbb889376e7e9bd1c13cf089b08e47057f0f
Kyle Ebertin <kyle78@ebertinhome.net>
2018-10-27 01:17:01 -0400

    j

--- public/js/dashboardApp.js
+++ public/js/dashboardApp.js
@@ -24,6 +24,12 @@ var dashboardApp = new Vue ({
             setTimeout( function() {
                 dashboardApp.setTabWidth();
             }, 500);
+        },
+        activeSite: function (val) {
+            setTimeout( function() {
+                console.log(dashboardApp.activeSite.lat);
+                console.log(dashboardApp.activeSite.lng);
+            }, 500);
         }
     },
     computed: {},
