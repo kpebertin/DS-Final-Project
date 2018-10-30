96fe6d51658792214e577ad0dc5dc7c4209745d5
shrbhatt <shrbhatt@indiana.edu>
2018-10-29 21:25:29 -0400

    Adding charting

--- public/js/dashboardApp.js
+++ public/js/dashboardApp.js
@@ -218,7 +218,7 @@ var dashboardApp = new Vue ({
             })
             .then( function(myJSON) {
                 dashboardApp.timeSeriesData = myJSON
-                //dashboardApp.buildOutputChart();
+                dashboardApp.buildOutputChart();
             })
             .catch( function(err) {
                 console.log("Error fetching time series data");
