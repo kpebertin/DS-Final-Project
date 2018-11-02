47dc58a64b3149c6f012864e7a63b9b279c8cffe
Lana La <lana@iu.edu>
2018-11-02 18:10:59 -0400

    Fixed comment spacing

--- app/email/email.php
+++ app/email/email.php
@@ -1,22 +1,29 @@
 <?php
 
-class ClientClass {
-    public $clientID;
-    
-    public function __construct($row) {
-        $this->clientID = isset($row['clientID']) ? $row['clientID'] : null;
-    }
-    
-    public static function getClientData() {
-        $db = new PDO(DB_SERVER, DB_USER, DB_PW);
-        $sql = 'SELECT * FROM Client;';
-        $pdoStatement = $db->prepare($sql);
-        $connection = $pdoStatement->execute([]);
-        $arrayOfClients = [];
-        while ($row = $pdoStatement->fetch(PDO::FETCH_ASSOC)) {
-            $aClient = new ClientClass($row);
-            array_push($arrayOfClients, $aClient);
-        }
-        return $arrayOfClients;
-    }
+class EmailProblem {
+	
+	public $clientID;
+	public $siteName;
+	public $primaryContact;
+	public $primaryContactEmail;
+	
+	public function __construct($row) {
+		$this->clientID = isset($row['clientID']) ? $row['clientID'] : null;
+		$this->siteName = isset($row['siteName']) ? $row['siteName'] : null;
+		$this->primarycontact = isset($row['primaryContact']) ? $row['primaryContact'] : null;
+		$this->primaryContactEmail = isset($row['primaryContactEmail']) ? $row['primaryContactEmail'] : null;
+	}
+	
+	public static function getClientData() {
+		$db = new PDO(DB_SERVER, DB_USER, DB_PW);
+		$sql = 'SELECT * FROM Client;';
+		$pdoStatement = $db->prepare($sql);
+		$connection = $pdoStatement->execute([]);
+		$arrayOfClients = [];
+		while ($row = $pdoStatement->fetch(PDO::FETCH_ASSOC)) {
+			$aClient = new ClientClass($row);
+			array_push($arrayOfClients, $aClient);
+		}
+		return $arrayOfClients;
+	}
 }
