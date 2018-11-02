99adf53d8a6140c561b0ab2e3401a46c528f2fe2
Lana La <lana@iu.edu>
2018-11-02 18:34:50 -0400

    Fixed comment spacing

--- app/email/emailDaily.py
+++ app/email/emailDaily.py
@@ -6,36 +6,36 @@ import json
 import urllib
 #######################################
 
-url = "file://email.php"
-req = urllib.Request(url, headers={'Content-type': 'application/json'})
-response = urllib.urlopen(req)
-the_page = response.read()
-print(the_page)
+# url = "file://email.php"
+# req = urllib.Request(url, headers={'Content-type': 'application/json'})
+# response = urllib.urlopen(req)
+# the_page = response.read()
+# print(the_page)
 
 #######################################
 
-# TO = 'kyle78@ebertinhome.net'
-# SUBJECT = 'TEST MAIL'
-# TEXT = 'Here is a message from python.'
+TO = 'kyle78@ebertinhome.net'
+SUBJECT = 'TEST MAIL'
+TEXT = 'Here is a message from python.'
 
-# # Gmail Sign In
-# gmail_sender = 'agsproject1@gmail.com'
-# gmail_passwd = 'kelleymsis'
+# Gmail Sign In
+gmail_sender = 'agsproject1@gmail.com'
+gmail_passwd = 'kelleymsis'
 
-# server = smtplib.SMTP('smtp.gmail.com', 587)
-# server.ehlo()
-# server.starttls()
-# server.login(gmail_sender, gmail_passwd)
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.ehlo()
+server.starttls()
+server.login(gmail_sender, gmail_passwd)
 
-# BODY = '\r\n'.join(['To: %s' % TO,
-# 					'From: %s' % gmail_sender,
-# 					'Subject: %s' % SUBJECT,
-# 					'', TEXT])
+BODY = '\r\n'.join(['To: %s' % TO,
+					'From: %s' % gmail_sender,
+					'Subject: %s' % SUBJECT,
+					'', TEXT])
 
-# try:
-# 	server.sendmail(gmail_sender, [TO], BODY)
-# 	print ('email sent')
-# except:
-# 	print ('error sending mail')
+try:
+	server.sendmail(gmail_sender, [TO], BODY)
+	print ('email sent')
+except:
+	print ('error sending mail')
 
-# server.quit()
+server.quit()
